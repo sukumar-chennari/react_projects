dd9c2cca3288fa7b81643f4f781258e07dc5af6c
sukumar-chennari <chennarisukumar@gmail.com>
2024-09-04 17:30:25 +0530

    updated the file

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@
 import './App.css'
 //git push --set-upstream origin aug-21 
 
-import { Recipe } from './Components/Axios/axiosfetch.jsx'
-import { FakeStore } from './Components/Axios/fakestore.jsx'
+
 import DisplayCards  from './Components/displayCards/displaycards.jsx'
 const App=()=>{
 return (
   <>
-    {/* <Recipe></Recipe> */}
+    
     <DisplayCards></DisplayCards>
   </>
   
