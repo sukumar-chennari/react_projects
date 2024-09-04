13fbc04fdb957a2979c8ca2c65439da989b36959
sukumar-chennari <chennarisukumar@gmail.com>
2024-09-04 17:34:10 +0530

    updated the file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@
 //import { CustomButton } from '@Button'
 
 import './App.css' 
-import { UseEffectExample } from './Components/hooks/useeffect/useeffect'
-import { UseEffectEx2 } from './Components/hooks/useeffect/useeffectEx2'
-import { FirstScreen } from './Components/nav10k/first'
+
 import { NavStack } from './Components/nav10k/navStack'
 
 const App=()=>{
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavigatorStack } from './Components/navigator/navigator-stack.jsx';
-import { NavStack } from './Components/nav10k/navStack.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
